3e268e9bcbd5bc9578300afcce6b5cf4d6819afc
Keshav Ladha <69904914+keshav-ladha@users.noreply.github.com>
2020-12-12 14:51:28 +0530

    Update news.py

--- news.py
+++ news.py
@@ -11,8 +11,8 @@ def speak(str):
 if __name__ == '__main__':
     import requests
     import json
-    url = ('http://newsapi.org/v2/top-headlines?country=in&apiKey=097b260e1646494b8afffe48d91ab55a')
-    n = int(input("No . of news Headlines"))
+    url = ('http://newsapi.org/v2/top-headlines?country=in&apiKey=<get your api key at newsapi.org>')
+    n = int(input("No . of news Headlines"))   #Input number of news you want to listen
     response = requests.get(url)
     text = response.text
     my_json = json.loads(text)
